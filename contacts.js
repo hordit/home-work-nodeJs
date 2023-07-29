4ed8d3e292b93c04ffe9e8fe01e2582cc91e09f5
Hordiyenko Tetyana <hordit@me.com>
2023-07-29 16:49:52 +0200

    add handlererror

--- contacts.js
+++ contacts.js
@@ -2,61 +2,55 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "db/contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const handlerError =
+  (func) =>
+  async (...args) => {
+    try {
+      return await func(...args);
+    } catch (error) {
+      throw new Error(`Error: ${error.message}`);
+    }
+  };
 
 const updateContacts = async (contacts) =>
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const listContacts = async () => {
-  try {
-    const data = await fs.readFile(contactsPath);
-    return JSON.parse(data);
-  } catch (error) {
-    throw new Error(`Error reading file: ${error.message}`);
-  }
-};
+const listContacts = handlerError(async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+});
 
-const getContactById = async (contactId) => {
-  try {
-    const contacts = await listContacts();
-    const result = contacts.find((item) => item.id === contactId);
-    return result || null;
-  } catch (error) {
-    throw new Error(`Error finding contact: ${error.message}`);
-  }
-};
+const getContactById = handlerError(async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+});
 
-const removeContact = async (contactId) => {
-  try {
-    const contacts = await listContacts();
-    const index = contacts.findIndex((item) => item.id === contactId);
-    if (index === -1) {
-      return null;
-    }
-    const [result] = contacts.splice(index, 1);
-    await updateContacts(contacts);
-    return result;
-  } catch (error) {
-    throw new Error(`Error removing contact: ${error.message}`);
+const removeContact = handlerError(async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
   }
-};
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+});
 
-const addContact = async (name, email, phone) => {
-  try {
-    const contacts = await listContacts();
-    const newContact = {
-      id: nanoid(),
-      name,
-      email,
-      phone,
-    };
-    const allContacts = [...contacts, newContact];
-    await updateContacts(allContacts);
-    return newContact;
-  } catch (error) {
-    throw new Error(`Error adding contact: ${error.message}`);
-  }
-};
+const addContact = handlerError(async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  const allContacts = [...contacts, newContact];
+  await updateContacts(allContacts);
+  return newContact;
+});
 
 module.exports = {
   listContacts,
